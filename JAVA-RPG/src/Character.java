73829175f8c74066b361fce762f38a133b24f036
Nikola Terran <nikolaterranthe1@gmail.com>
2017-12-04 18:46:23 -0500

    Changes to be committed: 	new file:   src/Character.java 	new file:   src/Gui.java 	new file:   src/Main.java 	new file:   src/java/rpg/Example.java 	new file:   src/java/rpg/Mkfile.java 	new file:   src/java/rpg/README.md 	new file:   src/java/rpg/manifest.mf
     Changes not staged for commit:
    	deleted:    ../Example.java
    	deleted:    ../Gui.java
    	deleted:    ../Main.java
    	deleted:    ../Mkfile.java
    	deleted:    ../README.md
    	deleted:    ../Role.java
    
     Untracked files:
    	../.gitignore
    	build.xml
    	nbproject/

---
+++ JAVA-RPG/src/Character.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+public class Character{
+
+	private String name;
+	private String role;
+	private String race;
+	private String alignment;
+	private String gender;
+
+	private int str;
+	private int dex;
+	private int cons;
+	private int intel;
+	private int wis;
+	private int cha;
+        
+        public Character(){}
+
+	public Character(int x, int y, int z, int a){		
+		
+		switch(x){
+			case 1: role = "wizard";
+					break;
+			
+		}
+		switch(y){
+			case 1: race = "human";
+					break;
+		}
+		switch(z){
+			case 1: alignment = "lawful";
+					break;
+		}
+		switch(z){
+			case 1: gender = "male";
+					break;
+		}
+                initialize(race);
+
+
+
+	}
+
+	private void initialize(String race){
+            if(race.equals("human")){
+                int int_max = 75;
+                Scanner assign = new Scanner(System.in);
+                System.out.println("Please assign your strength attribute point.");
+                str = Exception(assign.nextByte(),18);
+                System.out.println("Please assign your dexterity attribute point.");
+                dex = Exception(assign.nextByte(),18);
+                System.out.println("Please assign your constitution attribute point.");
+                cons = Exception(assign.nextByte(),18);
+                System.out.println("Please assign your intelligence attribute point.");
+                intel = Exception(assign.nextByte(),18);
+                System.out.println("Please assign your wisdom attribute point.");
+                wis = Exception(assign.nextByte(),18);
+                System.out.println("Please assign your charisma attribute point.");
+                cha = Exception(assign.nextByte(),18);
+                        } 
+	}
+        
+        private int Exception(int input, int expect){
+            while(true)
+                {if(input > expect){
+                    Scanner in = new Scanner(System.in);
+                 System.out.println("Your input value is larger than expected.");
+                 System.out.println("Please re-enter your input.");
+                    input = in.nextByte();
+                 
+            }
+            else{return input;}}
+        }           
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JAVA-RPG/src/Gui.java
@@ -0,0 +1,7 @@
+public class Gui{
+	
+	public static void title(){
+		System.out.println("Welcome to Java-DND.");
+		System.out.println("What is your name?");
+	}
+}
