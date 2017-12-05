2af9c1c256e344fb1f0bb6812e9994d41cc992b9
NikolaTerran <nikolaterranthe1@gmail.com>
2017-12-05 13:16:51 -0500

    Add files via upload

--- JAVA-RPG/src/Main.java
+++ JAVA-RPG/src/Main.java
@@ -1,9 +1,37 @@
 import java.util.Scanner;
+import java.io.*;
 public class Main{
 	
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);		
 		Gui.title();
+		
+        String fileName = in.next();
+        String line = null;
+
+        try {
+            FileReader fileReader = 
+                new FileReader(fileName);
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }   
+
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+			
+			
+			
+			
+            
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'"); }                 
+	
 	
 	}
 
